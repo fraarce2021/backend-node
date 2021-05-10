82f72cc8d2cabf31e852f8e40d19b441a7dc94ae
Francisco <danielarcesoftware@gmail.com>
2021-05-09 22:46:51 -0700

    body create

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router(); 
 
 let app = express();
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({extended: false}));
 app.use(router);
 
-router.get('/', (req,res)=>{
-    res.send('Hola desde ger');
+router.get('/message', (req,res)=>{
+    res.send('Lista de mensajes');
+});
+
+router.post('/message', (req,res)=>{
+    res.send('Mensaje añadido');
+});
+router.delete('/message', (req,res)=>{
+    console.log(req.query);
+    res.send(`Mensaje ${req.body.text} añadido correctamente`);
 });
 
 // app.use('/', (req, ress) => {
